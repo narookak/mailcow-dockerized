2fc15df952e9d601ae0da075281eb77a8e6a3ecf
Khushal Singh <khushal@gmail.com>
2022-03-02 19:55:36 +1300

    linux_arm64

---
+++ myinstall.bash
@@ -0,0 +1,14 @@
+curl -sSL https://get.docker.com/ | CHANNEL=stable sh
+
+systemctl enable --now docker
+
+wget https://github.com/docker/compose/releases/download/v2.2.3/docker-compose-linux-aarch64 -O /usr/local/bin/docker-compose
+chmod a+x /usr/local/bin/docker-compose
+
+cd /opt
+git clone https://github.com/narookak/mailcow-dockerized.git
+cd mailcow-dockerized
+./generate_config.sh 
+
+
+cp helper-scripts/docker-compose.override.yml.d/BUILD_FLAGS/docker-compose.override.yml .
